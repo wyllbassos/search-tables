e74eadd0df473882695f4f7faa75935a9f1c64c5
wyllbassos <bassoscolorado@gmail.com>
2021-05-06 23:16:51 -0300

    V0.4 -Ajustes useTabela

--- src/pages/ConsultaEmTabelas/hooks/tabelas.tsx
+++ src/pages/ConsultaEmTabelas/hooks/tabelas.tsx
@@ -1,26 +1,76 @@
-import React, { createContext, useContext, useState, useCallback } from 'react';
+import React, { createContext, useContext, useState, useCallback, useMemo } from 'react';
+import jsonTabelas from '../../../database/tabelas.json';
+import jsonProdutos from '../../../database/produtos.json';
+import jsonBens from '../../../database/bens.json';
 
-type ITabelasStateData = {};
+type CondicaoData = 'CONTEM' | 'CONTEM EXATO' | 'IGUAL' | 'COMECA';
 
-export interface ITabelasContextData extends ITabelasStateData {}
+type TabelasStateData = {
+  indexTabelaAtual: number;
+};
 
-const TabelasContext = createContext<ITabelasContextData>(
-  {} as ITabelasContextData
-);
+export interface TabelasContextData extends TabelasStateData {
+  tabelas: any[];
+  produtos: any[];
+  bens: any[];
+  limites: number[];
+  condicoes: CondicaoData[];
+  handleChangeTable: (index: number) => void;
+}
+
+type FiltroData = {
+  campo: string;
+  condicao: CondicaoData;
+  operacao: string;
+  texto: string;
+}
 
 type TabelasProviderProps = {
   children: React.ReactNode;
 };
 
+const data = (() => {
+  const strProdutos = JSON.stringify(jsonProdutos).toUpperCase();
+  const produtos = JSON.parse(strProdutos) as any[];
+
+  const strTabelas = JSON.stringify(jsonTabelas).toUpperCase();
+  const tabelas = JSON.parse(strTabelas) as any[];
+
+  const strBens = JSON.stringify(jsonBens).toUpperCase();
+  const bens = JSON.parse(strBens) as any[];
+
+  return { produtos, tabelas, bens };
+})();
+
+const limites = [10, 50, 100, 500, 1000, 5000];
+const condicoes: CondicaoData[] = ['CONTEM', 'CONTEM EXATO', 'IGUAL', 'COMECA'];
+
+const TabelasContext = createContext<TabelasContextData>(
+  {} as TabelasContextData
+);
+
 export const TabelasProvider: React.FC<TabelasProviderProps> = ({
   children,
 }: TabelasProviderProps) => {
-  const [state, setState] = useState<ITabelasStateData>({});
+  const [state, setState] = useState<TabelasStateData>({
+    indexTabelaAtual: 0,
+  });
+
+  const handleChangeTable = useCallback((index: number) => {
+    setState(current => ({
+      ...current,
+      indexTabelaAtual: index,
+    }));
+  }, [])
 
   return (
     <TabelasContext.Provider
       value={{
         ...state,
+        ...data,
+        limites,
+        condicoes, 
+        handleChangeTable,
       }}
     >
       {children}
@@ -28,7 +78,7 @@ export const TabelasProvider: React.FC<TabelasProviderProps> = ({
   );
 };
 
-export const useTabelas = (): ITabelasContextData => {
+export const useTabelas = (): TabelasContextData => {
   const context = useContext(TabelasContext);
 
   if (!context) {
